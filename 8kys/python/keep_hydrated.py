0b7a19c8dce30bd2f03f429ff44492d5423f7e9f
Jennifer <jenniferchen@pneumasoftware.com>
2020-11-18 22:04:10 +0800

    Initial commit

---
+++ 8kys/python/keep_hydrated.py
@@ -0,0 +1,29 @@
+"""
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+"""
+
+
+def litres(time):
+    water = time / 2
+    return int(water)
+
+
+print(litres(2))  # 'should return 1 litre'
+print(litres(1.4))  # 'should return 0 litres'
+print(litres(12.3))  # 'should return 6 litres'
+print(litres(0.82))  # 'should return 0 litres'
+print(litres(11.8))  # 'should return 5 litres'
+print(litres(1787))  # 'should return 893 litres'
+print(litres(0))  # 'should return 0 litres'
