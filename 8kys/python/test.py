ce40de36624bb71aa7347deef07f437a1df08756
Jennifer0606 <jennifer.c914137@gmail.com>
2020-11-22 22:40:55 +0800

    update 1

---
+++ 8kys/python/test.py
@@ -0,0 +1,11 @@
+input_arr = [0, 7, 5, 5, 6, 7, 100, 55]
+
+
+new_arr = sorted(list(set(input_arr)))
+
+for i in range((len(input_arr) - len(new_arr))):
+    new_arr.append(new_arr[-1]+1)
+
+
+print(new_arr)
+
